eec12652d44618f26c81faa6779f1e2e81253a99
Tricell25 <99977880+mrsmite1@users.noreply.github.com>
2024-04-27 18:47:52 +0200

    add step size (not working well)

--- code/data.py
+++ code/data.py
@@ -1,7 +1,31 @@
 import csv
 import requests
 
-def get_route_coordinates(api_key, origin, destination):
+def main():
+
+    api_key = 'api'
+    origin = 'Central Park, New York'
+    destination = 'East Village, New York, État de New York, États-Unis'
+    STEP_SIZE_FT = 200 # Consume quickly the api if small
+
+    # Récupération des coordonnées le long de l'itinéraire
+    route_coordinates = get_route_coordinates(api_key, origin, destination, STEP_SIZE_FT)
+    if route_coordinates:
+        print("Coordonnées le long de l'itinéraire :")
+        for i, coord in enumerate(route_coordinates, start=1):
+            print(f"Étape {i}: Latitude = {coord[0]}, Longitude = {coord[1]}")
+
+        # Écrire les coordonnées dans un fichier CSV
+        write_to_csv('coordinates.csv', route_coordinates)
+        print("Les coordonnées ont été écrites dans le fichier coordinates.csv.")
+
+        # Récupération des images Google Street View et des images satellites
+        get_street_view_images(api_key, route_coordinates)
+        print("Les images Google Street View et satellites ont été récupérées.")
+
+
+
+def get_route_coordinates(api_key, origin, destination, STEP_SIZE_FT):
     url = 'https://maps.googleapis.com/maps/api/directions/json'
     params = {
         'origin': origin,
@@ -19,11 +43,17 @@ def get_route_coordinates(api_key, origin, destination):
 
         for step in steps:
             start_location = step['start_location']
-            coordinates.append((start_location['lat'], start_location['lng']))
+            end_location = step['end_location']
+
+            distance = step['distance']['value']
+
+            interpolated_coords = interpolate_coordinates(start_location, end_location, distance, STEP_SIZE_FT)
+
+            coordinates.extend(interpolated_coords)
 
         # Ajouter la dernière étape (destination)
-        end_location = steps[-1]['end_location']
-        coordinates.append((end_location['lat'], end_location['lng']))
+        last_step_end_location = steps[-1]['end_location']
+        coordinates.append((last_step_end_location['lat'], last_step_end_location['lng']))
 
         return coordinates
     else:
@@ -36,20 +66,22 @@ def get_street_view_images(api_key, coordinates):
     for i, coord in enumerate(coordinates, start=1):
         lat, lng = coord
         # Récupération de l'image Street View
-        street_view_url = f"https://maps.googleapis.com/maps/api/streetview?size=600x400&location={lat},{lng}&key={api_key}"
+        street_view_url = f"https://maps.googleapis.com/maps/api/streetview?size=1920x1080&location={lat},{lng}&key={api_key}"
         street_view_response = requests.get(street_view_url)
         if street_view_response.status_code == 200:
             with open(f"streetview_{i}.jpg", 'wb') as f:
                 f.write(street_view_response.content)
+                print(f"{i}: Image street view aux coordonnéeslat:{lat} lng:{lng} enregistrée")
         else:
             print(f"Impossible de récupérer l'image Street View pour la coordonnée {lat}, {lng}")
 
         # Récupération de l'image satellite
-        satellite_url = f"https://maps.googleapis.com/maps/api/staticmap?center={lat},{lng}&zoom=17&size=600x400&maptype=satellite&key={api_key}"
+        satellite_url = f"https://maps.googleapis.com/maps/api/staticmap?center={lat},{lng}&zoom=17&size=1920x1080&maptype=satellite&key={api_key}"
         satellite_response = requests.get(satellite_url)
         if satellite_response.status_code == 200:
             with open(f"satellite_{i}.jpg", 'wb') as f:
                 f.write(satellite_response.content)
+                print(f"{i}: Image satellite aux coordonnées lat:{lat} lng:{lng} enregistrée")
         else:
             print(f"Impossible de récupérer l'image satellite pour la coordonnée {lat}, {lng}")
 
@@ -61,28 +93,23 @@ def write_to_csv(filename, data):
         for item in data:
             csv_writer.writerow(item)
 
-def main():
-    # Insérez votre clé API Google Maps
-    api_key = 'clé API'
+def interpolate_coordinates(start, end, distance, step_size):
+    units = step_size/3.28
+    segments = 1 if distance/units < 1 else int(distance/units)
+    # Calculer les deltas latitudinaux et longitudinaux
+    delta_lat = (end['lat'] - start['lat']) / segments
+    delta_lng = (end['lng'] - start['lng']) / segments
 
-    # Adresses de départ et d'arrivée
-    origin = 'Central Park, New York'
-    destination = 'East Village, New York, État de New York, États-Unis'
+    # Générer les coordonnées intermédiaires
+    interpolated_coordinates = []
+    for i in range(segments):
+        lat = round(start['lat'] + i * delta_lat, 7)
+        lng = round(start['lng'] + i * delta_lng, 7)
+        interpolated_coordinates.append((lat, lng))
 
-    # Récupération des coordonnées le long de l'itinéraire
-    route_coordinates = get_route_coordinates(api_key, origin, destination)
-    if route_coordinates:
-        print("Coordonnées le long de l'itinéraire :")
-        for i, coord in enumerate(route_coordinates, start=1):
-            print(f"Étape {i}: Latitude = {coord[0]}, Longitude = {coord[1]}")
 
-        # Écrire les coordonnées dans un fichier CSV
-        write_to_csv('coordinates.csv', route_coordinates)
-        print("Les coordonnées ont été écrites dans le fichier coordinates.csv.")
+    return interpolated_coordinates
 
-        # Récupération des images Google Street View et des images satellites
-        get_street_view_images(api_key, route_coordinates)
-        print("Les images Google Street View et satellites ont été récupérées.")
 
 if __name__ == "__main__":
     main()
